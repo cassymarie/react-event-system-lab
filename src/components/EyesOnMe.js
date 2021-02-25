b97b574769f6831286eecc730d138d4757b3697c
cassymarie <cassy2205@gmail.com>
2021-02-25 14:55:41 -0700

    commit tree 7e86adf85cfb8ff86f2f1d34e0aec67fc85b6af5

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,33 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+class EyesOnMe extends React.Component {
+  constructor() {
+    super();
+
+    // Define the initial state:
+    this.state = {
+      
+    };
+  }
+
+  handleFocus = () => {
+    // Update our state here...
+    console.log(`Good!`)
+  };
+
+  handleBlur = () => {
+    // Update our state here...
+    console.log(`Hey! Eyes on me!`)
+  };
+
+  render() {
+    return (
+      <div>
+        <button onFocus={this.handleFocus} onBlur={this.handleBlur}></button>
+      </div>
+    );
+  }
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,27 @@
-// Code Keypad Component Here
+
+import React from 'react';
+
+class Keypad extends React.Component {
+  constructor() {
+    super();
+
+    // Define the initial state:
+    this.state = {
+      
+    };
+  }
+
+  handleEvent = (e) => {
+    console.log(`Entering password...`)
+  };
+
+  render() {
+    return (
+      <div>
+        <input type="password" onKeyUp={this.handleEvent}></input>
+      </div>
+    );
+  }
+}
+
+export default Keypad;
